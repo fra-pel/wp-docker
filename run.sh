1aa49f94aebfa4e44a69d7658cecc24006d0ef95
Francesco Pellegrino <fra.pel@gmail.com>
2022-05-14 14:31:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 nome=`sed -n -e '/COMPOSE_PROJECT_NAME/ s/.*\= *//p' .env`
-echo ${nome}
-echo $nome
 sed -i 's/db_/'db_"$nome"'/' docker-compose.yml
 sed -i 's/wp__/'wp_"$nome"'/' docker-compose.yml
 echo "OK TERMINATO"
+echo "############[ AVVIO ]##############"
+docker-compose up -d --build
